836bdebd31124b31dcd6748b97ad90b55784cf54
tiasmra <sitimariamtia@gmail.com>
2026-05-24 17:28:07 +0700

    Feat: Tugas 2 Praktikum Struktur Data

--- src/KodeQuickSort.java
+++ src/KodeQuickSort.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class KodeQuickSort {
+    // kode ini untuk mengurutkan array menggunakn algoritma Quick Sort secara rekursif
     public static void quickSort(int[] array, int low, int high) {
         if (low < high) {
             int pivotIndex = partition(array, low, high);
@@ -10,11 +11,12 @@ public class KodeQuickSort {
         }
     }
 
+    // kode ini untuk membagi array berdasarkan pivot dan mengembalikan posisi pivot
     public static int partition(int[] array, int low, int high) {
         int pivot = array[high];
         int i = low - 1;
 
-        for(int j = low;j < high; j++) {
+        for(int j = low; j < high; j++) {
             if(array[j] <= pivot) {
                 i++;
 
@@ -23,24 +25,25 @@ public class KodeQuickSort {
                 array[j] = temp;
             }
         }
-
         int temp = array[i + 1];
         array[i + 1] = array[high];
         array[high] = temp;
         return i + 1;
     }
 
+    // kode ini untuk menmpilkan isi array
     public static void printArray(int[] array) {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void main (String[] args) {
-        int[] arr = {101, 102, 103, 104, 105, 106, 107, 108};
-        System.out.println("Array asli:");
+    // kode ini merupakan fungsi utama untuk menjalankan program Quick Sort
+    public static void main(String[] args) {
+        int[] arr = {105, 107, 102, 104, 108, 106, 101, 103};
+        System.out.println("Array aslinya:");
         printArray(arr);
 
         quickSort(arr, 0, arr.length - 1);
-        System.out.println("Array yang telah diurutkan:");
+        System.out.println("Array yang diurutkan:");
         printArray(arr);
     }
 }
